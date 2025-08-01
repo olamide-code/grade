6aebb447f423710c1ad51978c7b48fdcad1e2859
olamide-code <olamidewrite@gmail.com>
2025-08-01 16:59:15 +0100

    A simple python quiz app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-#Python Code for calculating or determining the grade of student
+This is a simple python programme quiz for student.
+It contains a total of 4 questions and three options, we calculate the score and display the result.
--- main.py
+++ main.py
@@ -1,22 +1,87 @@
-# A PYTHON CODE FOR A SCHOOL SCORE GRADER
-class Student:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
+# A PYPTHON PROGRAM TO SOLVE SIMPLE QUIZ
+print("WELCOME TO PYTHON SIMPLE QUIZ PLATFORM/APP")
+print("You are given 4 questions and 3 options,select only the correct option\n")
 
-    def grade(self):
-        if self.score >= 70:
-            print("your grade is A")
-        elif 60 <= self.score < 70:
-            print("your grade is B")
-        elif 50<= self.score < 60:
-            print("your grade is C" )
+running = True
+import string
+
+while running:
+    user_choice = input("To exit this program, kindly enter ('q' to quit) or press ENTER to continue \n")
+    if user_choice.lower() == 'q':
+        print("GOOD BYE!! YOU EXITED FROM THE PROGRAM")
+        break
+
+    try:
+        score = 0
+
+        # Question 1
+        print("1. what is the capital of Nigeria")
+        print("A. Nigeria")
+        print("B. Abuja")
+        print("C. Calabar")
+        answer1 = input("Choose an option: ").strip().upper()
+        if answer1 == 'B':
+            print("✅✅ CORRECT!! \n")
+            score += 1
         else:
-            print("your grade is F")
+            print("❌❌ Wrong!! Try again")
 
-name = input("Enter your name: \n")
-score =int(input("Enter your score: \n"))
+        # Question 2
+        print("2. Which country hosted the 17th BRICS Summit on July 6, 2025 ?: ")
+        print("A. USA")
+        print("B. Brazil")
+        print("C. Canada")
+        answer2 = input("select an option \n").strip().upper()
+        if answer2 == 'B':
+            print("✅✅ YOU ARE DOING GOOD!!")
+            score += 1
+        else:
+            print("❌❌ Wrong choice, Try Agian!!")
 
-my_grade = Student(name , score)
+        # Question 3
+        print("3. What was the IMF's global growth forecast for 2025 ? \n")
+        print("A. Raised to 3.0%")
+        print("B. Below 3.0%")
+        print("C. Raised to 2.5%")
+        answer3 = input("Choose an option: ").strip().upper()
+        if answer3 == 'A':
+            print("✅✅ CORRECT!!")
+            score += 1
+        else:
+            print("❌❌ Try Again!!")
 
-my_grade.grade()
+        # Question 4
+        print("4. what does CPU stand for ?")
+        print("A. Central Personal Unit")
+        print("B. Computer Processing Unit")
+        print("C. Centreal processing Unit")
+        answer4 = input("Choose an option: ").strip().upper()
+        if answer4 == 'C':
+            print("✅✅ CORRECR!!")
+            score += 1
+        else:
+            print("❌❌ Wrong Choice")
+
+        # Final Score
+        print(f"Your Final score is: {score}/4")
+
+        if score == 4:
+            print("👏👏👏 Excellent!! You got all correct")
+
+        elif score == 3:
+            print("👏👏 you did well")
+
+        elif score == 2:
+            print("👏 you tried")
+
+        elif score == 1:
+            print("👎 practice more next time")
+
+        else:
+            print("👎👎 you perform poorly. practice harder next time")
+
+
+
+    except ValueError:
+        print("Invalid input")
+
